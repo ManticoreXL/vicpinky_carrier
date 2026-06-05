04cb06740e7f3ad0ef1700c2a4351dd897fa2c48
0307102bj41-afk <0307102bj41@gmail.com@example.com>
2026-06-05 16:45:59 +0900

    사람 인식 패키지 업로드

---
+++ src/turtlebot_people_tracker/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'turtlebot_people_tracker'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'people_detector = turtlebot_people_tracker.people_detector:main',
+        ],
+    },
+)
