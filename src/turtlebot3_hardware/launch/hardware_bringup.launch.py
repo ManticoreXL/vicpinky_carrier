c908aa75b8339f1f4b7dac0d36aea466326365d9
ManticoreXL <zip3366@naver.com>
2026-06-15 13:37:30 +0900

    bot-id 인자로 주게 수정

--- src/turtlebot3_hardware/launch/hardware_bringup.launch.py
+++ src/turtlebot3_hardware/launch/hardware_bringup.launch.py
@@ -1,9 +1,17 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    bot_id_arg = DeclareLaunchArgument(
+        'bot_id',
+        default_value='tb3_01',
+        description='ID of the robot (e.g., tb3_01, tb3_02, vicpinky)'
+    )
+
     headlight_node = Node(
         package='turtlebot3_hardware',
         executable='headlight_node',
@@ -31,10 +39,14 @@ def generate_launch_description():
         executable='turtlebot3_webrtc',
         name='turtlebot3_webrtc',
         output='screen',
-        arguments=['--server', 'http://10.10.14.70:3001']
+        arguments=[
+            '--server', 'http://10.10.14.70:3001',
+            '--bot-id', LaunchConfiguration('bot_id')
+        ]
     )
 
     return LaunchDescription([
+        bot_id_arg, 
         headlight_node,
         voice_node,
         rosbridge_node,
